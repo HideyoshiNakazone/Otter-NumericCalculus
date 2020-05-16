4ea9205b6592c2de6a383743c1caf8d8c89865e0
Hideyoshi <vitor.h.n.batista@gmail.com>
2020-05-16 14:58:59 -0300

    v1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Otter", # Replace with your own username
+    name="otter-nc", # Replace with your own username
     version="1.0",
     author="Vitor Hideyoshi",
     author_email="vitor.h.n.batista@gmail.com",
